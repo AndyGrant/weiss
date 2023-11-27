49ce9c4c3443cc73825b4871797e0b0a62eff454
andrew <andrew.github@grantnet.us>
2023-11-27 15:38:57 -0500

    Bench 32148915

--- src/search.c
+++ src/search.c
@@ -454,7 +454,7 @@ static int AlphaBeta(Thread *thread, Stack *ss, int alpha, int beta, Depth depth
 
             // Cut if the reduced depth search beats the threshold
             if (score >= probCutBeta)
-                return score - ProbCutReturn;
+                return MAX(beta, score - ProbCutReturn); // Must be at least beta for the cutoff (!)
         }
     }
 
